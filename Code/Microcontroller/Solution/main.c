4225d105530a341bf546e21b182ecaec9c888a25
StjerneIdioten <dragonest7@gmail.com>
2016-03-30 23:46:23 +0200

    Created a code composer project for the microcontroller. Defining the basic structure of things and including a few files

---
+++ Code/Microcontroller/Solution/main.c
@@ -0,0 +1,47 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Robot Technology 4th Semester
+*
+* MODULENAME.: main.c
+*
+* PROJECT....: 4th Semester Project
+*
+* DESCRIPTION: The main file running the entire program
+*
+/***************************** Include files *******************************/
+#include <tm4c123gh6pm.h>
+#include <stdint.h>
+#include "Modules/EMP/emp_type.h"
+#include "Modules/SysTick/systick.h"
+#include "Modules/GPIO/GPIO.h"
+#include <UART/uart0.h>
+#include <EMP/emp_type.h>
+
+/*****************************    Defines    *******************************/
+
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Variables   *******************************/
+
+/*****************************   Functions   *******************************/
+
+int main(void)
+
+{
+	//Initialization
+	disable_global_int();
+	SysTick_init();
+	GPIO_init();
+	UART0_init(19200, 8, 1, 0);
+	enable_global_int();
+
+	while(1) {
+
+	}
+
+	return (0);
+}
+
+/****************************** End Of Module *******************************/
+
