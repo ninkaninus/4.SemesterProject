c7da1416d0f6cd2e2a36154a20f9af2234e50be5
Gugfann <kmolha@gmail.com>
2016-04-19 22:15:20 +0200

    moar

--- Tests/Motor/Step Response/Captures/Step.m
+++ Tests/Motor/Step Response/Captures/Step.m
@@ -46,7 +46,50 @@ plot(Time4,W4);
 
 legend('No Belt','Belt On','Belt+Frame On','Belt+Frame+Cam On','location','southeast')
 
-T1 = time_constant(650,W1,Time1);
-T2 = time_constant(605,W2,Time2);
-T3 = time_constant(600,W3,Time3);
-T4 = time_constant(590,W4,Time4);
+%%
+T1 = time_constant(650,W1,Time1); K1 = 650;
+T2 = time_constant(605,W2,Time2); K2 = 605;
+T3 = time_constant(600,W3,Time3); K3 = 600;
+T4 = time_constant(590,W4,Time4); K4 = 590;
+
+sys1 = tf(K1,[T1 1]);
+sys2 = tf(K2,[T2 1]);
+sys3 = tf(K3,[T3 1]);
+sys4 = tf(K4,[T4 1]);
+
+[W11,Time11] = step(sys1,4);
+[W21,Time21] = step(sys2,4);
+[W31,Time31] = step(sys3,4);
+[W41,Time41] = step(sys4,4);
+
+figure(1)
+subplot(2,1,1)
+plot(Time1,W1);
+ylim([0,750])
+subplot(2,1,2)
+plot(Time11,W11);
+ylim([0,750])
+
+figure(2)
+subplot(2,1,1)
+plot(Time2,W2);
+ylim([0,750])
+subplot(2,1,2)
+plot(Time21,W21);
+ylim([0,750])
+
+figure(3)
+subplot(2,1,1)
+plot(Time3,W3);
+ylim([0,750])
+subplot(2,1,2)
+plot(Time31,W31);
+ylim([0,750])
+
+figure(4)
+subplot(2,1,1)
+plot(Time4,W4);
+ylim([0,750])
+subplot(2,1,2)
+plot(Time41,W41);
+ylim([0,750])
