1be218246ca4a60482eb32b8e9c769540791bc81
Yubh8n <firestorm_sjoveren@hotmail.com>
2016-05-05 16:16:22 +0200

    Menu filer tilføjet, er kun skelet til at starte med. Chris fortsætter en anden dag xD

---
+++ Code/Microcontroller/FreeRTOS_Solution/Modules/Menu/Menu.c
@@ -0,0 +1,37 @@
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded Programming (EMP)
+*
+* MODULENAME.: Menu.c
+*
+* PROJECT....: Pan/Tilt Project
+*
+* DESCRIPTION: See module specification file (.h-file).
+*
+* Change Log:
+******************************************************************************
+* Date    Id    Change
+* YYMMDD
+* --------------------
+* 050516  CBM   Module created.
+*
+*****************************************************************************/
+
+/***************************** Include files *******************************/
+#include <stdint.h>
+#include <tm4c123gh6pm.h>
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+#include "Modules/EMP/emp_type.h"
+#include "Modules/LCD/lcd.h"
+#include "Modules/Tasking/tmodel.h"
+#include "Modules/Tasking/messages.h"
+
+/*****************************    Defines    *******************************/
+/*****************************   Constants   *******************************/
+/*****************************   Variables   *******************************/
+/*****************************   Functions   *******************************/
+/****************************** End Of Module ******************************/
