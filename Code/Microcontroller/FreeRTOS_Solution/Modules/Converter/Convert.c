c231c235d5bcb664a02ec0073e59bb6ea52c86ee
DanielHviid <danielholsthviid@gmail.com>
2016-05-11 12:06:07 +0200

    stuff

--- Code/Microcontroller/FreeRTOS_Solution/Modules/Converter/Convert.c
+++ Code/Microcontroller/FreeRTOS_Solution/Modules/Converter/Convert.c
@@ -32,6 +32,7 @@
 
 /*****************************    Defines    *******************************/
 
+\defien INDEX			2048
 #define INDEX_PAN		2048//1778
 #define INDEX_TILT		2048//1778
 #define OFFSET			280
