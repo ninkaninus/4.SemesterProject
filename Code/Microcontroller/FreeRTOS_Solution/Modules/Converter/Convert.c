449d9e3f09059df9329f3e8ec38d99a3d5481ec7
DanielHviid <danielholsthviid@gmail.com>
2016-05-11 11:27:50 +0200

    Reconfigured

--- Code/Microcontroller/FreeRTOS_Solution/Modules/Converter/Convert.c
+++ Code/Microcontroller/FreeRTOS_Solution/Modules/Converter/Convert.c
@@ -32,15 +32,16 @@
 
 /*****************************    Defines    *******************************/
 
-#define INDEX_PAN		1778
-#define INDEX_TILT		1778
+#define INDEX_PAN		2048//1778
+#define INDEX_TILT		2048//1778
+#define OFFSET			280
 #define PAN_DIRECTION_OFFSET 0
 #define	THETA_1			20
 #define	THETA_2			160
 #define THETA_3			200
 #define	THETA_4			340
-#define TILT_MIN		20
-#define TILT_MAX		340
+#define TILT_MIN		250
+#define TILT_MAX		290
 
 
 /*****************************   Constants   *******************************/
@@ -53,12 +54,18 @@ extern xSemaphoreHandle coordinate_access_sem;
 
 void convert_and_secure(void)
 {
+	INT32U pan = 	get_msg_state(SSM_SP_DEG_PAN);
+	INT32U tilt =	get_msg_state(SSM_SP_DEG_TILT);
 
-	//if(xSemaphoreTake(coordinate_access_sem, 100))
-	//{
-		INT32U pan = 	get_msg_state(SSM_SP_DEG_PAN);
-		INT32U tilt =	get_msg_state(SSM_SP_DEG_TILT);
+	if ((tilt > TILT_MIN && tilt < TILT_MAX) || pan < THETA_1 || (pan > THETA_2 && pan < THETA_3) || pan > THETA_4)
+	{
+		tilt = INDEX_TILT;
+		pan = INDEX_PAN;
 
+	}
+	else
+	{
+		// Pan transformation
 		if (pan >= 270)
 		{
 			pan = pan-270;
@@ -67,7 +74,7 @@ void convert_and_secure(void)
 		{
 			pan = pan+90;
 		}
-
+		// Tilt transformation
 		if (tilt >= 270)
 		{
 			tilt = tilt-270;
@@ -76,30 +83,11 @@ void convert_and_secure(void)
 		{
 			tilt = tilt+90;
 		}
-
-		if (tilt < TILT_MIN || tilt > TILT_MAX)
+		// Flip hvis pan er større end 180 grader
+		if (pan > 180)
 		{
-			tilt = INDEX_TILT;
-			pan = INDEX_PAN;
-			put_msg_state(SSM_SP_TILT,tilt);
-			put_msg_state(SSM_SP_PAN,pan);
-			//xSemaphoreGive(coordinate_access_sem);
-			return;
-		}
-
-		if(pan > 180)
-		{
-			if(pan < THETA_3 || pan < THETA_4)
-			{
-				tilt = INDEX_TILT;
-				pan = INDEX_PAN;
-				put_msg_state(SSM_SP_TILT,tilt);
-				put_msg_state(SSM_SP_PAN,pan);
-				//xSemaphoreGive(coordinate_access_sem);
-				return;
-			}
-
 			pan = pan-180;
+
 			if(tilt < 180)
 			{
 				tilt = tilt - 2*(tilt-90);
@@ -108,25 +96,15 @@ void convert_and_secure(void)
 			{
 				tilt = tilt + 2*(90-tilt);
 			}
-
 		}
-		else if(pan < THETA_1 || pan > THETA_2)
-		{
-			tilt = INDEX_TILT;
-			pan = INDEX_PAN;
-			put_msg_state(SSM_SP_TILT,tilt);
-			put_msg_state(SSM_SP_PAN,pan);
-			//xSemaphoreGive(coordinate_access_sem);
-			return;
-		}
-		tilt = 3 * tilt + INDEX_TILT;
-		pan = 3 * pan + INDEX_PAN + PAN_DIRECTION_OFFSET;
 
-		put_msg_state(SSM_SP_TILT,tilt);
-		put_msg_state(SSM_SP_PAN,pan);
+		tilt = 3 * tilt + INDEX_TILT- OFFSET;
+		pan = 3 * pan + INDEX_PAN - OFFSET + PAN_DIRECTION_OFFSET;
 
-		//xSemaphoreGive(coordinate_access_sem);
-	//}
+	}
+
+	put_msg_state(SSM_SP_TILT,tilt);
+	put_msg_state(SSM_SP_PAN,pan);
 }
 
 /****************************** End Of Module *******************************/
