718b1609be1d489bc2c76cfd629c19c057e2d331
Gugfann <kmolha@gmail.com>
2016-05-19 23:02:10 +0200

    Mere test

--- Tests/Motor/Step Response/Captures/Step_balanced.m
+++ Tests/Motor/Step Response/Captures/Step_balanced.m
@@ -6,9 +6,6 @@ load('Pan-Tilt/Pan Motor.mat')
 [Time1,W1] = combine_vectors(digital_channel_0,digital_channel_1,digital_sample_rate_hz);
 
 ratio = ratio_vector(W1,12);
-
-
-
 W1 = fit_ratio(ratio,W1);
 
 figure(5)
@@ -19,7 +16,7 @@ grid on;
 
 %%
 
-load('Pan-Tilt/Pan Cam Balanced 5.mat')
+load('Pan-Tilt/Pan Cam Balanced 4.mat')
 [Time1,W1] = combine_vectors(digital_channel_0,digital_channel_1,digital_sample_rate_hz);
 
 W1 = fit_ratio(ratio,W1);
