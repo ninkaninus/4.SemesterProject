cf7599e3ea24f1d46d4150e7254ea26342546826
StjerneIdioten <dragonest7@gmail.com>
2016-05-23 20:27:33 +0200

    Updated some tle's

--- Code/Tracker/Tracker.py
+++ Code/Tracker/Tracker.py
@@ -22,8 +22,8 @@ home.elevation = 15 # meters
 # Always get the latest ISS TLE data from:
 # http://spaceflight.nasa.gov/realdata/sightings/SSapplications/Post/JavaSSOP/orbit/ISS/SVPOST.html
 iss = ephem.readtle('ISS',
-     '1 25544U 98067A   16141.62816457  .00016717  00000-0  10270-3 0  9032',
-     '2 25544  51.6405 195.5226 0001738 107.9306 252.2036 15.54677601   695'
+     '1 25544U 98067A   16144.52075938  .00016717  00000-0  10270-3 0  9035',
+     '2 25544  51.6408 181.0913 0001653 118.9223 241.2096 15.54667329  1148'
 )
 
 pcSat = ephem.readtle('PCSAT',
@@ -31,19 +31,22 @@ pcSat = ephem.readtle('PCSAT',
      '2 26931  67.0527  27.3826 0007086 262.9280  97.1015 14.30406980764014'
 )
 
-tiSat1 = ephem.readtle('TISAT1',
-     '1 36799U 10035E   16141.11196898  .00001251  00000-0  13642-3 0  9997',
-     '2 36799  98.0277 250.9616 0012879 171.7734 188.3697 14.89772933317247'
+cosmos2504 = ephem.readtle('COSMOS2504',
+     '1 40555U 15020D   16143.80913921 +.00000063 +00000-0 +20631-3 0  9999',
+     '2 40555 082.4864 143.5120 0215717 012.7184 347.9341 12.80502650053654'
 )
 
-beeSat2 = ephem.readtle('BEESAT2',
-     '1 39136U 13015G   16141.17435292  .00003036  00000-0  17901-3 0  9991',
-     '2 39136  64.8733  58.7697 0007773 267.9007  92.1227 15.13522545169767'
+brite = ephem.readtle('brite',
+     '1 39431U 13066R   16143.91707026 +.00000114 +00000-0 +35042-4 0  9997',
+     '2 39431 097.6784 151.1551 0201626 244.0827 113.9465 14.46716276131824'
 )
 
+cutie = ephem.readtle('cutie',
+     '1 27844U 03031E   16144.54349662  .00000101  00000-0  65456-4 0  9999',
+     '2 27844  98.6926 153.1836 0010492  40.2184 319.9768 14.21948798668948'
+)
 
-itemToTrack = beeSat2
-
+itemToTrack = iss
 ser = serial.Serial('COM7', 115200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, 0)  # open serial port
 print(ser.name)  # check which port was really used
 
