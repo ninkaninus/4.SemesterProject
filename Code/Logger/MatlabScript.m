c8989f808842fafbaca9e6ebc6ee70d10d079c22
StjerneIdioten <dragonest7@gmail.com>
2016-05-24 14:02:02 +0200

    Making some positions responses and comparing them with the modelled response.

--- Code/Logger/MatlabScript.m
+++ Code/Logger/MatlabScript.m
@@ -1,6 +1,16 @@
-fileID = load('90To270.txt');
+fileID = load('270To90Both100.txt');
 time = fileID(:,1);
 pan = fileID(:,2);
-tilt= fileID(:,3);
+tilt = fileID(:,3);
 
+fileID1 = load('270To90Both200.txt');
+time1 = fileID1(:,1);
+pan1 = fileID1(:,2);
+tilt1 = fileID1(:,3);
+
+figure(1)
+subplot(2,1,1)
 plot(time,[pan tilt])
+
+subplot(2,1,2)
+plot(time1,[pan1 tilt1])
